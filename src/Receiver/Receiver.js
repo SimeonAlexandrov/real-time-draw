29c8bda8c8743f536af910c347ce832fe4c942fa
SimeonAlexandrov <simeon.alexandrov17@gmail.com>
2019-09-05 18:34:27 +0300

    Updated style of receiver.

--- src/Receiver/Receiver.js
+++ src/Receiver/Receiver.js
@@ -32,28 +32,40 @@ class Receiver extends Component {
         <h2>Hi, {this.props.clientId}. Try to guess the drawing</h2>
         <br/>
         <div style={{width:"100%"}}>
-          <div style={{ display: "table",margin: "0 auto", border: "1px solid black"}}>
-            <CanvasDraw
-              ref={canvas => this.canvas = canvas}
-              saveData={this.state.saveData}
-            />
-          </div>
-          <Row gutter={16} style={{marginBottom: "16px"}}>
-              <Col span={12}>
+          <Row gutter={16}>
+            <Col span={8}/>
+            <Col span={8}>
+              <div style={{ display: "table",margin: "0 auto", border: "1px solid black"}}>
+                <CanvasDraw
+                  ref={canvas => this.canvas = canvas}
+                  saveData={this.state.saveData}
+                  disabled
+                />
+            </div>
+            </Col>
+            <Col span={8}/>
+          </Row>
+          <Row gutter={16} style={{marginTop: "16px", marginBottom: "16px"}}>
+              <Col span={8}/>
+              <Col span={4} style={{textAlign: "center"}}>
                 <Button>{this.props.round.LabelOptions[0]}</Button>
               </Col>
-              <Col span={12}>
+              <Col span={4} style={{textAlign: "center"}}>
                 <Button>{this.props.round.LabelOptions[1]}</Button>
               </Col>
+              <Col span={8}/>
             </Row>
             <Row gutter={16} style={{marginBottom: "16px"}}>
-              <Col span={12}>
+              <Col span={8}/>
+              <Col span={4} style={{textAlign: "center"}}>
                 <Button>{this.props.round.LabelOptions[2]}</Button>
               </Col>
-              <Col span={12}>
+              <Col span={4} style={{textAlign: "center"}}>
                 <Button>{this.props.round.LabelOptions[3]}</Button>
               </Col>
+              <Col span={8}/>
             </Row>
+            
         </div>
       </Page>
     );
